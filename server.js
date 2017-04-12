2b0bdbc179f56820a1a13b16d56ecfbaf3928214
Nusrath <nusrathyasin000@gmail.com>
2017-04-12 19:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,67 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.post('/create-user', function (req, res) {
+    var username=req.body.username;
+     var password=req.body.password;
+    var salt=crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username, dbString],function (err,result){
+         if (err){
+            res.status(500).send(err.toString());
+        }else {
+            res.send('User successfully created: '+ username);
+        }
+        
+    });
+});
+app.post('/login', function (req, res) {
+    var username=req.body.username;
+     var password=req.body.password;
+   
+    pool.query('SELECT * FROM "user" WHERE username=$1',[username],function (err,result){
+         if (err){
+            res.status(500).send(err.toString());
+        }else {
+            if (result.rows.length===0){
+                res.send(403).send('usename/password is invalid');
+            }else {
+                var dbString=result.rows[0].password;
+               var salt=dbString.split('$')[2];
+               var hashedPassword=hash(password,salt);
+               if(hashedPassword===dbString){
+                    req.session.auth = {userId: result.rows[0].id};
+               res.send('credentials correct!'); 
+               }else {
+                    res.send(403).send('usename/password is invalid');
+               
+               }
+            }
+           
+        }
+        
+    });
+});
 
+app.get('/check-login', function (req, res) {
+   if (req.session && req.session.auth && req.session.auth.userId) {
+       // Load the user object
+       pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
+});
+
+app.get('/logout', function (req, res) {
+   delete req.session.auth;
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
